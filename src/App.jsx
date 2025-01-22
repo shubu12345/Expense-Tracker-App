9691d09906e2c545cfca9c6ffa49f3bde78e72a3
LL\shushind <shubham.shinde@publicisapient.com>
2025-01-22 22:29:18 +0530

    css updated

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import ExpenseData from "./components/ExpenseData";
 import ExpenseForm from "./components/ExpenseForm";
 import ExpenseTable from "./components/ExpenseTable";
 import { useLocalStorage } from "./Hooks/useLocalStorage";
+import logo from "./assets/10384161.png";
 
 function App() {
   const [expense, setExpense] = useLocalStorage("expense", {
@@ -17,7 +18,10 @@ function App() {
   return (
     <>
       <main>
-        <h1>Track Your Expense</h1>
+        <div className="logo-container">
+          <h1>Track Your Expense</h1>
+          <img src={logo} width="80" height="80" alt="expense" />
+        </div>
         <div className="expense-tracker">
           <ExpenseForm
             setExpenses={setExpenses}
